ce7e3c309b7d947c76352551365f68608fb5ddb1
Insang Song <insang.song@nih.gov>
2024-01-02 11:41:46 -0500

    pgis init + data import sql

--- postgis/init-pg.sh
+++ postgis/init-pg.sh
@@ -9,5 +9,6 @@ pg_ctl -D "$PGDATA" -l $POSTGRESQL_SERVER -w -U "$POSTGRESQL_USER" -P "$POSTGRES
 
 createdb -O "$POSTGRESQL_USER" "$POSTGRESQL_DATABASE"
 
+raster2pgsql -s 5070 -I -C -M -t 200x200 ~/Documents/srtm15nc.tif srtm_nc | psql -d pgfelix
 
 # EOF
